37cb0061e1417473617ccad23519cf5ff94bc973
frameshft <adiletkadyrov@gmail.com>
2017-07-10 17:55:56 +0600

    Add PostsShow component

---
+++ src/components/posts_show.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
+class PostsShow extends Component {
+  componentDidMount() {
+    // Provided by React Router (?_?)
+    this.props.match.params.id;
+    this.props.fetchPost();
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ posts }) {
+
+}
+
+export default connect(null, { fetchPost })(PostsShow);
