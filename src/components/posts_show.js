ba5c338f3442b6b135d73896494be41136343595
frameshft <adiletkadyrov@gmail.com>
2017-07-14 16:01:38 +0600

    Add conditional rendering to PostShow

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -5,21 +5,29 @@ import { fetchPost } from '../actions';
 class PostsShow extends Component {
   componentDidMount() {
     // Provided by React Router (?_?)
-    this.props.match.params.id;
-    this.props.fetchPost();
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-function mapStateToProps({ posts }) {
-
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
